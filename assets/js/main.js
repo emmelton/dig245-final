22a4b68ac1c77ced2e27ff5bf738e12389137d1b
Emma Melton <emmelton@davidson.edu>
2021-11-07 17:04:40 -0500

    commit 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,20 @@
 
 /* javascript */
+
+document.querySelectorAll('#commentForm input');
+Array.form(document.querySelectorAll('#commentForm input')).reduce((acc, input) => ({ ...acc, [input.id]: input.value }), {});
+
+
+function game() {
+
+  let str = '';
+  let r = Math.floor(Math.random() * data.length);
+  str += `var name = ${data[r].time}`
+
+
+  var name = "Pieter";
+console.log(name.replace("good", "bad"));
+
+var input = "[input from user]";
+var ourIntriguingString = ourInterestingString.replace("interesting", "intriguing");
+console.log(ourIntriguingString);
